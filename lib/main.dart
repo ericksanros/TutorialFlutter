fbbe15cc05432bf9af3ee2f740e7b46f3b8fc370
ericksanr <sistemasbarragan@oxschool.edu.mx>
2021-05-12 17:11:42 -0500

    last step of first level

--- lib/main.dart
+++ lib/main.dart
@@ -9,45 +9,55 @@ import 'package:flutter/rendering.dart'; //INCLUDE ENGLISH_WORDS PACKAGE FROM PU
 void main() => runApp(
     MyApp()); //WE USE THE ARROW SYMBOL FOR ONE-LINE FUNTIONS OR METHOD,void does not return,
 
-class MyApp extends StatelessWidget {
-  //STATELESSWIDGET MAKE THE APP ITSELF A WIDGET
+class MyApp extends StatelessWidget {  //STATELESSWIDGET MAKE THE APP ITSELF A WIDGET
   @override
-  Widget build(BuildContext context) {
-    //Constructor build, takes buildcontext as argument
-    final wordPair = WordPair
-        .random(); //New final object, cant change the value, Wordpair gets random words from english_words
-    return MaterialApp(
-      //MaterialApp configures the top level navigator
+  Widget build(BuildContext context) {    //Constructor build, takes buildcontext as argument
+   // final wordPair = WordPair.random(); //New final object, cant change the value, Wordpair gets random words from english_words
+    return MaterialApp(      //MaterialApp configures the top level navigator
       title: 'Texto de prueba', // tittle of navigator
-      home: Scaffold(
-        //Class that develop the basic material design visual layout structure
-        appBar: AppBar(
-          //Toolbar
+      home: Scaffold(        //Class that develop the basic material design visual layout structure
+        appBar: AppBar(          //Toolbar
           title: Text('My first App'), //Texto en titulo de navbar
           backgroundColor: Color(0xFF455A64),
-          centerTitle: true, //tituli centrado?
+          centerTitle: true, //titulo centrado?
         ),
-        body: Center(
-          //alineation of body
-          child: Text(wordPair
-              .asPascalCase), //PascalCase == upper camel case, means that each word in the string, including the first one
+        body: Center(         //alineation of body
+          child: RandomWords(), //call of class Randomwords, that generate random words in english
         ),
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  //creation of a stateful widget, this requires 2 clasess.by adding stful and the IDE does the rest of the code
+class RandomWords extends StatefulWidget {//creation of a stateful widget, this requires 2 clasess.by adding stful and the IDE does the rest of the code
   @override
   _RandomWordsState createState() => _RandomWordsState(); //
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  //second class of stateful widget, this one save the list of words generated before by randomwords
-  @override
-  Widget build(BuildContext context) {
-    // widget builder
-    return Container(); //give back a container
-  }
+class _RandomWordsState extends State<RandomWords> {//second class of stateful widget, this one save the list of words generated before by randomwords
+ @override
+ final _suggestions = <WordPair>[]; //to save the words generated 
+ final _biggerFont = TextStyle(fontSize: 18.0); //Make the font bigger.
+
+    Widget build(BuildContext context) { //constructor to show the random word generated
+      final wordPair = WordPair.random();
+      return Text(wordPair.asPascalCase);
+    }
+
+    Widget _buildSuggestions() {
+  return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+      });
 }
+
+}
+
+
